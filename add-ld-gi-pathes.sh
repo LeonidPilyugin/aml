4e513ebcc08cc0c5b13cce61872c28a5ea203e71
Leonid Pilyugin <l.pilyugin04@gmail.com>
2025-02-11 11:40:27 +0300

    Fix add-ld-gi-pathes.sh

--- add-ld-gi-pathes.sh
+++ add-ld-gi-pathes.sh
@@ -1,4 +1,4 @@
 for subdir in build/aml/*; do
     export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)/$subdir
-    export GI_TYPELIB_PATH=$LD_LIBRARY_PATH:$(pwd)/$subdir
+    export GI_TYPELIB_PATH=$GI_TYPELIB_PATH:$(pwd)/$subdir
 done
